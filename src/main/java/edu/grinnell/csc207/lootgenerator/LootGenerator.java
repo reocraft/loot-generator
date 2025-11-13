1da369d4d45428ea7150078b211aaaba556d96c9
reocraft <reocraft.creeper@gmail.com>
2025-11-13 11:09:54 -0600

    Can kill random monsters now

--- src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
@@ -1,11 +1,48 @@
 package edu.grinnell.csc207.lootgenerator;
+import java.util.*;
+import java.io.*;
 
 public class LootGenerator {
     /** The path to the dataset (either the small or large set). */
     private static final String DATA_SET = "data/small";
+    private ArrayList<Monsters> monsters;
+    private String monsterFile = DATA_SET + "/monstats.txt";
+    public LootGenerator() {
+        monsters = new ArrayList<Monsters>();
+        loadMonsters(monsterFile);
+    }
+
+    public void loadMonsters(String monsterFile) {
+        try (Scanner sc = new Scanner(new File(monsterFile))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine().trim();
+                String[] parts = line.split("\t");
+                String name = parts[0];
+                String type = parts[1];
+                int level = Integer.parseInt(parts[2]);
+                String treasureClass = parts[3];
+                Monsters monster = new Monsters(name, type, level, treasureClass);
+                monsters.add(monster);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: Monster file not found.");
+        }
+    }
+
+    public void run() {
+        if (monsters.isEmpty()) {
+            System.out.println("No monsters loaded.");
+            return;
+        }
+        Random rand = new Random();
+        Monsters randomMonster = monsters.get(rand.nextInt(monsters.size()));
+        System.out.println("You killed a " + randomMonster.getName() + "!");
+    }
+
     
     public static void main(String[] args) {
         System.out.println("This program kills monsters and generates loot!");
-        // TOOD: Implement me!
+        LootGenerator lg = new LootGenerator();
+        lg.run();
     }
 }
